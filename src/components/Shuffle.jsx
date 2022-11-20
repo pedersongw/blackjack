9b9c87bdc27b021f5f569f6a984efffae0d94082
Garrett Pederson <garrettpederson@garretts-mini.myfiosgateway.com>
2022-11-20 14:01:03 -0500

    especially proud of this: function to find random points on a circle wide enough to be offscreen. react spring to move them all to a center point in a shuffling fashion

--- src/components/Shuffle.jsx
+++ src/components/Shuffle.jsx
@@ -1,46 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, createElement } from "react";
 import { useSprings, animated } from "@react-spring/web";
 import styles from "./Shuffle.module.css";
 
 const Shuffle = () => {
+  const [hk, setHk] = useState({});
   const [coords, setCoords] = useState([]);
-  for (let i = 0; i < 8; i++) {
-    let xPos = Math.sqrt(Math.pow(100, 2) - Math.pow());
-  }
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    let radius =
+      window.innerWidth < window.innerHeight
+        ? window.innerHeight
+        : window.innerWidth;
+    const { x: h, y: k } = ref.current.getBoundingClientRect();
+    let viewportAdjustedH = (h / window.innerWidth) * 100;
+    let viewportAdjustedK = (k / window.innerHeight) * 100;
+
+    setHk({ h: viewportAdjustedH, k: viewportAdjustedK });
+
+    let yDiff = radius + k - (k - radius);
+    let coords = [];
+
+    for (let i = 0; i < 52; i++) {
+      let newY = Math.random() * yDiff + (k - radius);
+      let x = Math.sqrt(Math.pow(radius, 2) - Math.pow(newY - k, 2));
+      let newX =
+        Math.random() >= 0.5
+          ? ((x + h) / window.innerWidth) * 100
+          : ((-Math.abs(x) + h) / window.innerWidth) * 100;
+      let newYvH = (newY / window.innerHeight) * 100;
+
+      coords.push([newX, newYvH]);
+    }
+    console.log(coords);
+    setCoords(coords);
+  }, []);
+
+  const [springs, api] = useSprings(
+    coords.length,
+    (index) => ({
+      xy: coords[index],
+    }),
+    []
+  );
+
+  const shuffleCenter = () => {
+    for (let i = coords.length; i > 0; i--) {
+      console.log(i);
+      setTimeout(() => {
+        api.current[i].start({ xy: [hk["h"], hk["k"]] });
+      }, i * 100);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div
-        className={styles.dot}
-        style={{ left: coords[0][0], top: coords[0][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[1][0], top: coords[1][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[2][0], top: coords[2][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[3][0], top: coords[3][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[4][0], top: coords[4][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[5][0], top: coords[5][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[6][0], top: coords[6][1] }}
-      ></div>
-      <div
-        className={styles.outerDot}
-        style={{ left: coords[7][0], top: coords[7][1] }}
-      ></div>
+      <div id={styles.dot} ref={ref}></div>
+      {springs.map((anim, index) => (
+        <animated.div
+          className={styles.dot}
+          key={index}
+          style={{
+            transform: springs[index].xy.to(
+              (x, y) => `translate3d(${x}vw, ${y}vh, 0)`
+            ),
+          }}
+        />
+      ))}
+      <button onClick={() => shuffleCenter()}>Button</button>
     </div>
   );
 };
